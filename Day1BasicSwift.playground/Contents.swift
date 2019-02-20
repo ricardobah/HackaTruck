15b3e5e042ef98ae25c31764564fd0c96838bdc4
student <student@student16.local>
2019-02-20 20:35:26 -0300

    OO

--- Day1BasicSwift.playground/Contents.swift
+++ Day1BasicSwift.playground/Contents.swift
@@ -135,3 +135,136 @@ enum Status: Int {
     case Bloqueado = 2
 }
 
+
+
+var statusUsuario: Status = Status.Ativo
+
+print(statusUsuario)
+
+switch(statusUsuario) {
+case .Ativo:
+    print("Usuário ativo! :-)")
+case .Inativo:
+    print("Atenção! Usuário inativo.")
+case .Bloqueado:
+    print("Usuário bloqueado :-(")
+}
+
+
+
+class Humano {
+    // Propriedades armazenadas
+    let nome: String
+    let sobrenome: String
+    var idade: Int
+    
+    // Propriedades computadas
+    var nomeCompleto: String {
+        return "\(self.nome) \(self.sobrenome)"
+    }
+    
+    //  Construtor
+    init(nome: String, sobrenome: String, idade: Int) {
+        // 'self' = this
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.idade = idade
+    }
+    
+    // Método
+    func falar() -> String {
+        return "Olá, meu nome é \(self.nomeCompleto) e eu tenho \(self.idade) anos."
+    }
+    
+    // Método de classe -> da p usar static e chamar sem instanciar
+    static func isMamifero() -> Bool {
+        return true
+    }
+}
+
+// Herança
+// Apenas herança simples
+// Após o nomes da classe, coloque ':' e o nome da classe de onde herdar
+class Atleta: Humano {
+    var esporte: String
+    
+    init(nome: String, sobrenome: String, idade: Int, esporte: String) {
+        self.esporte = esporte
+        // Inicializador da classe pai
+        super.init(nome: nome, sobrenome: sobrenome, idade: idade)
+    }
+    
+    // Sobrescrita do método de falar do Humano
+    override func falar() -> String {
+        return "Meu nome é \(self.nomeCompleto), \(self.idade) anos, e eu pratico \(self.esporte)."
+    }
+    
+    // Método do atleta apra praticar esporte
+    func praticarEsporte() -> String {
+        return "\(self.nome) está praticando \(self.esporte)."
+    }
+}
+
+// Instanciado novos objetos
+let human = Humano(nome: "Jon", sobrenome: "Doe", idade: 24)
+let atlet = Atleta(nome: "Mary", sobrenome: "Jane", idade: 20, esporte: "curling")
+
+// Vamos testar se um Atleta é um Humano
+func isAtleta(pessoa: Humano) -> Bool {
+    return pessoa is Atleta
+}
+
+isAtleta(pessoa: human)
+isAtleta(pessoa: atlet)
+
+// Os dois podem falar, pois os dois são Humanos
+human.falar()
+atlet.falar()
+
+// Mas apenas o Atleta pratica esportes
+atlet.praticarEsporte()
+
+
+
+protocol TriAtleta {
+    var energia: Int { get set }
+    
+    func correr() -> String
+    func nadar() -> String
+    func pedalar() -> String
+}
+
+
+class Pessoa: Atleta, TriAtleta {
+    // Temos que implementar as propriedades que o protocolo pede
+    var energia: Int = 10
+    
+    // E implementar os métodos do protocolo
+    func correr() -> String {
+        return "\(self.nome): Estou correndo!"
+    }
+    
+    func nadar() -> String {
+        return "\(self.nome): Estou nadando!"
+    }
+    
+    func pedalar() -> String {
+        return String("\(self.nome): Estou pedalando!")
+    }
+}
+
+// Vamos testar agora
+// Criamos uma função que recebe uma pessoa pra disputar o triatlo
+func iniciarTriatlo(pessoa: Pessoa) {
+    pessoa.correr()
+    pessoa.nadar()
+    pessoa.pedalar()
+}
+
+// Instaciamos um objeto da classe Pessoa
+let thiago: Pessoa = Pessoa(nome: "Thiago", sobrenome: "Pereira", idade: 28, esporte: "natação")
+
+// Iniciamos o triatlo com essa pessoa
+iniciarTriatlo(pessoa: thiago)
+
+
