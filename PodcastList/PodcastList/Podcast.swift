c17c30b4833558a9eca51cfec307f5ef2ebacdc4
ricardobah <rickribeiro99@gmail.com>
2019-02-22 20:21:12 -0300

    podcast project

---
+++ PodcastList/PodcastList/Podcast.swift
@@ -0,0 +1,19 @@
+//
+//  Podcast.swift
+//  PodcastList
+//
+//  Created by student on 22/02/19.
+//  Copyright © 2019 Ricardo Ribeiro. All rights reserved.
+//
+
+import Foundation
+
+class Podcast{
+
+
+
+
+
+
+}
+
---
+++ PodcastList/PodcastList/PodcastTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PodcastTableViewCell.swift
+//  PodcastList
+//
+//  Created by student on 22/02/19.
+//  Copyright © 2019 Ricardo Ribeiro. All rights reserved.
+//
+
+import UIKit
+
+class PodcastTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
